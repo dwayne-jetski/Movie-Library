006eb23fd3fcd6ee7ea35ecebfb8e794d089cfa2
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2020-11-19 20:19:06 -0600

    got server started and added quick start for server for npm start

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
+
+
+app.get("/", (req, res) =>{
+
+    res.send("what is up?");
+
+});
+
+app.listen(5000, function(){
+    console.log("This is the server;");
+
+});
