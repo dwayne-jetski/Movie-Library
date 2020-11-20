a773a342b38ae0cad44ccb724c42d8f105c50f30
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2020-11-19 20:43:24 -0600

    linked repository to index.js and added function to get data through the repository's functions.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const { response } = require('express');
+const  response  = require('express');
+const repoContext = require('./repository/repository-wrapper')
 const express = require('express');
 const app = express();
 
+//gets all movie data
+app.get("/data/movies", (req, res) => {
+    let movies = repoContext.movies.findAllMovies();
+    res.send(movies);
+});
 
 app.get("/", (req, res) =>{
 
