360d01d26da5071ca281f6fd56af541d048f9f92
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2020-11-25 19:07:11 -0600

    commit before pulling updated info

--- Client/app.js
+++ Client/app.js
@@ -9,15 +9,6 @@
     }
 });
 
-$.ajax({
-    url: "http://localhost:5000/data/movies",
-    dataType: "json",
-    type: "put",
-    success: function(response){
-        
-    }
-});
-
 //buildCards(data);
 function buildCards(data){
     
@@ -25,8 +16,7 @@ function buildCards(data){
 
     for(var i = 0; i < data.length; i++){
 
-<<<<<<< HEAD
-        if(data[i].hasOwnProperty('image') === true){
+        if(data[i].hasOwnProperty('image') == true){
             $(`#movie-tiles`).append(
 
                 `   <button class = "col text-center col-sm-3 card-styles " type="button" class="btn btn-primary" data-toggle="modal" data-target="#${data[i].id}Modal>  
@@ -88,43 +78,6 @@ function buildCards(data){
                         </div>
                     </div>`)
         }     
-=======
-        
-        $(`#movie-tiles`).append(
-
-            `<button class = "col text-center card container col-sm-3 card-styles " type="button" class="btn btn-primary" data-toggle="modal" data-target="#${data[i].id}Modal">  
-                <div class="card-image-format">
-                    <img class="card-img-top" src="https://images.unsplash.com/photo-1594908900066-3f47337549d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80">
-                </div>
-                <div class="card-body">
-                    <h2 class="card-title font-weight-bold">${data[i].title}</h2>
-                </div>
-            </button>
-            
-            <div class="modal fade" id="${data[i].id}Modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">${data[i].title}</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <h7>${data[i].director}</h7>
-                        <h7>${data[i].genre}</h7>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Update</button>
-                    </div>
-                </div>
-            </div>
-        </div>`
-            
-        )
-           
->>>>>>> 829c5ed62e8daf2a34ca877448ef9c9b70fcaebf
     }  
 } 
 
@@ -155,25 +108,24 @@ function userInputSearch(apiData){
     buildCards(apiData)
 
     $('index.html').ready(()=>{
+        //event listener for search bar
+        $('#user-input').on(`keyup`, function(){
+            var value = $(this).val();
+            if(value != null){
+                var data = searchCards(value, apiData)
+                buildCards(data);
+            }
+            
+        });
 
-    $('#user-input').on(`keyup`, function(){
-        var value = $(this).val();
-        if(value != null){
-            var data = searchCards(value, apiData)
-            buildCards(data);
-        }
         
-    }
 
-    );
-
-    //event listener for modal to create new movie
-
-    $('#add-movie-submit').on('click', ()=>{
-           postMovie();
-        }); 
+        //event listener for modal to create new movie
+        $('#add-movie-submit').on('click', ()=>{
+            postMovie();
+            }); 
     
-});
+    });
 };
 
 
@@ -212,5 +164,4 @@ function createMovieObject(response){
 
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('click')
-})
-
+  })
